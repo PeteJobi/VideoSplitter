f5b4863a7402a3b2f6adf518535683554647b4f0
Peter Egunjobi <masterpdon@gmail.com>
2025-07-26 19:36:39 +0100

    Initial commit (with sub modules)

---
+++ VideoSplitter/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Core;
+using Windows.Storage.Pickers;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace VideoSplitter
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private string ffmpegLocation;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void MainPage_OnDrop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+                GoToTrimmer(items[0].Path);
+            }
+        }
+
+        private void MainPage_OnDragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+        }
+
+        private async void ShowFilePicker(object sender, RoutedEventArgs e)
+        {
+            string[] allSupportedTypes = [".mkv", ".mp4"];
+            var filePicker = new FileOpenPicker();
+            filePicker.SuggestedStartLocation = PickerLocationId.VideosLibrary;
+            foreach (var supportedType in allSupportedTypes)
+            {
+                filePicker.FileTypeFilter.Add(supportedType);
+            }
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(MainWindow.Window);
+            WinRT.Interop.InitializeWithWindow.Initialize(filePicker, hwnd);
+            var file = await filePicker.PickSingleFileAsync();
+            GoToTrimmer(file.Path);
+        }
+
+        private async void GoToTrimmer(string videoPath)
+        {
+            var ffmpegPath = string.Empty;
+            try
+            {
+                ffmpegPath = Path.Join(Package.Current.InstalledLocation.Path, "Assets/ffmpeg.exe");
+            }
+            catch (InvalidOperationException)
+            {
+                ffmpegPath = "Assets/ffmpeg.exe";
+            }
+            if (!File.Exists(ffmpegPath))
+            {
+                await ErrorDialog.ShowAsync();
+                return;
+            }
+            Frame.Navigate(typeof(VideoSplitterPage), new SplitterProps { FfmpegPath = ffmpegPath, VideoPath = videoPath });
+        }
+    }
+}
